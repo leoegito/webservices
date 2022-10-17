ebdd196b2019b8a3ac3377e98f9521f32ece4d00
Leonardo Egito <leoegitoadm@gmail.com>
2022-10-17 00:08:21 -0300

    Exception Handler

--- src/main/java/com/leoegito/webservices/services/UserService.java
+++ src/main/java/com/leoegito/webservices/services/UserService.java
@@ -2,6 +2,7 @@ package com.leoegito.webservices.services;
 
 import com.leoegito.webservices.entities.User;
 import com.leoegito.webservices.repositories.UserRepository;
+import com.leoegito.webservices.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
