5715f2fb573f1b0d8d33a2305714fbaa823276eb
Leonardo Egito <leoegitoadm@gmail.com>
2022-10-18 22:25:07 -0300

    Exceptions

--- src/main/java/com/leoegito/webservices/services/UserService.java
+++ src/main/java/com/leoegito/webservices/services/UserService.java
@@ -2,10 +2,14 @@ package com.leoegito.webservices.services;
 
 import com.leoegito.webservices.entities.User;
 import com.leoegito.webservices.repositories.UserRepository;
+import com.leoegito.webservices.services.exceptions.DatabaseException;
 import com.leoegito.webservices.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
@@ -29,13 +33,24 @@ public class UserService {
     }
 
     public void delete(Long id){
-        repository.deleteById(id);
+        try{
+            repository.deleteById(id);
+        } catch(EmptyResultDataAccessException e){
+           throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
+
     }
 
     public User update(Long id, User obj){
-        User entity = repository.getById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
